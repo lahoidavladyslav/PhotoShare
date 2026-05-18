8423030d7bbf2725800c57b743380213a844dc89
lahoidavladyslav <lahoydav@gmail.com>
2026-05-18 15:12:53 +0300

    Auth, depend

---
+++ src/api/dependencies.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.config import settings
+from src.db.database import get_db
+from src.db.models import User
+from src.repository.users import get_user_by_email
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme), 
+    db: AsyncSession = Depends(get_db)
+) -> User:
+    """Декодує токен і повертає поточного користувача."""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = await get_user_by_email(email, db)
+    if user is None or not user.is_active:
+        raise credentials_exception
+        
+    return user
